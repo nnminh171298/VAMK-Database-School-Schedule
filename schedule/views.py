2e35ccbda9e8d31e11af0707de29937d647068da
nnminh171298 <ngocminh171298@gmail.com>
2018-02-28 21:00:31 +0200

    add requirements

--- schedule/views.py
+++ schedule/views.py
@@ -8,7 +8,6 @@ from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import viewsets
-from django.views.generic.base import ContextMixin
 
 #Course
 class CourseView(generic.ListView):
